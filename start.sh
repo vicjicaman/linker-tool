9bc22d9282d0a5880eb69340373bc164cf1da012
Victor Jimenez <vic.jicama@gmail.com>
2019-12-19 05:44:21 +0100

    Mod bootstrap to latest version

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 export LOCAL_WORKSPACE=~/repoflow/linker/home
 export LOCAL_GRAPH_BOOTSTRAP_SERVICE_PORT=16005
 
-export LOCAL_VERSION_BOOTSTRAP=1.70.3-master #module @nebulario/linker-local-bootstrap-graph
+export LOCAL_VERSION_BOOTSTRAP=1.70.2-master #module @nebulario/linker-local-bootstrap-graph
 export LOCAL_VERSION_GRAPH=1.70.4-master #module repoflow/linker-local-handler-graph-container
 export LOCAL_VERSION_WEB=1.70.4-master #module repoflow/linker-local-handler-web-container
-export LOCAL_VERSION_WORKER=1.70.0-master #module repoflow/linker-local-worker-graph-container
+export LOCAL_VERSION_WORKER=1.70.1-master #module repoflow/linker-local-worker-graph-container
 
 echo "Starting..."
 echo "{\"dependencies\":{\"@nebulario/linker-local-bootstrap-graph\":\"${LOCAL_VERSION_BOOTSTRAP}\"}}" > package.json
